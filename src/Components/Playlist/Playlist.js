452c25a575147abc999928be34128f1cb3c13903
wxomi <elif.alyoruk@ozu.edu.tr>
2022-09-07 02:12:01 +0530

    28 section 1

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Playlist.css';
+import TrackList from '../TrackList/TrackList';
 
 class Playlist extends React.Component{
     render(){
         return(
-            <div class="Playlist">
+            <div className="Playlist">
             <input value="New Playlist"/>
             {/* <!-- Add a TrackList component --> */}
-            <button class="Playlist-save">SAVE TO SPOTIFY</button>
+            <TrackList></TrackList>
+            <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
     }
--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './SearchResults.css';
+import TrackList from '../TrackList/TrackList';
 
 class SearchResults extends React.Component {
     render(){
@@ -7,6 +8,7 @@ class SearchResults extends React.Component {
             <div className="SearchResults">
             <h2>Results</h2>
             {/* <!-- Add a TrackList component --> */}
+            <TrackList></TrackList>
             </div>
         )
     }
